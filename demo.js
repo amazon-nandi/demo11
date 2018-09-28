91a7456510562c14bfbcceb1d0718b727242dfe2
Nandi <amazon.nandi@gmail.com>
2018-09-28 19:58:54 -0400

    push

---
+++ demo.js
@@ -0,0 +1,12 @@
+const exp = require('express');
+var oExp = new exp();
+// var oRoute = oExp.Router();
+
+oExp.use('/abcd',(req,res,next) => {
+    next();
+    res.send('hi');
+
+});
+
+const oPort = process.env.port || 3000;
+oExp.listen(oPort);
